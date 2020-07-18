27bfac8c13f438372adf625420203323fd50e1d9
trimbakeshwar <hudge.trimbakeshwar@gmail.com>
2020-07-18 15:37:32 +0530

    Added:-on change metod

--- src/components/registration.jsx
+++ src/components/registration.jsx
@@ -3,6 +3,7 @@ import { TextField, Button } from '@material-ui/core';
 import VisibilityIcon from '@material-ui/icons/Visibility';
 import VisibilityOffIcon from '@material-ui/icons/VisibilityOff';
 import "../stylepage/registrations.scss"
+
 export class Registration extends Component {
     constructor(props) {
         super(props);
@@ -16,19 +17,29 @@ export class Registration extends Component {
         }
     }
     firstNameHandler = (e) => {
-      
+        console.log("first name", e.target.value);
+        this.setState({ firstName: e.target.value, firstNameError: "" });
+        console.log("data after setste first name", this.state);
     }
     lastNameHandler = (e) => {
-      
+        console.log("lastName", e.target.value);
+        this.setState({ lastName: e.target.value, lastNameError: "" });
+        console.log("data after setste last name", this.state);
     }
     emailHandler = (e) => {
-      
+        console.log("email", e.target.value);
+        this.setState({ email: e.target.value, EmailError: "" });
+        console.log("data after setste email", this.state);
     }
     passwordHandler = (e) => {
-        
+        console.log("data", e.target.value);
+        this.setState({ password: e.target.value, passwordError: "", MissmatchError: "" });
+        console.log("data after setste password", this.state);
     }
     confirmPasswordHandler = (e) => {
-      
+        console.log("data", e.target.value);
+        this.setState({ confirmPassword: e.target.value, confirmPasswordError: "", MissmatchError: "" });
+        console.log("data after setste confirm pass", this.state);
     }
   
     visableIconHandler = eve => {
@@ -93,6 +104,12 @@ export class Registration extends Component {
                 <Button id="contained-button" variant="contained" color="primary" float='right' onClick={this.Register}>submit</Button>
             </div>
         </div>
+        <div className='imageContainer'>
+            <div className='imgcontainer'>
+                <img className='image' />
+            </div>
+        </div>
+
        
     </div>
 );

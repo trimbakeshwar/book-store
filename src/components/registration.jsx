b065ac3e995464d982feb536d76abe8368d353ab
trimbakeshwar <hudge.trimbakeshwar@gmail.com>
2020-07-18 18:10:07 +0530

    Added:-validation for registration page

--- src/components/registration.jsx
+++ src/components/registration.jsx
@@ -4,6 +4,7 @@ import VisibilityIcon from '@material-ui/icons/Visibility';
 import VisibilityOffIcon from '@material-ui/icons/VisibilityOff';
 import "../stylepage/registrations.scss"
 import userservice from "../services/userservices";
+import patterns from "../configration/regex";
 const service = new userservice();
 export class Registration extends Component {
     constructor(props) {
@@ -11,10 +12,17 @@ export class Registration extends Component {
         this.state = {
             firstName: "",
             lastName: "",
+            firstNameError: "",
+            lastNameError: "",
             email: "",
+            EmailError: "",
             password: "",
             confirmPassword: "",
+            passwordError: "",
+            confirmPasswordError: "",
+            MissmatchError:"",
             visability: false,
+
         }
     }
     firstNameHandler = (e) => {
@@ -42,13 +50,37 @@ export class Registration extends Component {
         this.setState({ confirmPassword: e.target.value, confirmPasswordError: "", MissmatchError: "" });
         console.log("data after setste confirm pass", this.state);
     }
-  
+
     visableIconHandler = eve => {
         this.state.visability ?
             this.setState({ visability: false })
             : this.setState({ visability: true })
     }
     Register = (e) => {
+        if (!patterns.NamePattern.test(this.state.firstName)) {
+            this.setState({ firstNameError: "invalid first name" })
+        }
+        if (!patterns.NamePattern.test(this.state.lastName)) {
+            this.setState({ lastNameError: "invalid last name" })
+        }
+        if (!patterns.EmailPattern.test(this.state.email)) {
+            this.setState({ EmailError: "invalid email" })
+        }
+        if (!patterns.passwordPattern.test(this.state.password)) {
+            this.setState({ passwordError: "invalid password" })
+        }
+        if (!patterns.passwordPattern.test(this.state.confirmPassword)) {
+            this.setState({ confirmPasswordError: "invalid password" })
+        }
+        if (this.state.password != this.state.confirmPassword) {
+            this.setState({ MissmatchError: "password and confirm Password is not same" })
+        }
+        if ((patterns.NamePattern.test(this.state.firstName)) && (patterns.NamePattern.test(this.state.lastName)) &&
+            (patterns.EmailPattern.test(this.state.email)) && (patterns.passwordPattern.test(this.state.password)) &&
+            (patterns.passwordPattern.test(this.state.confirmPassword)) && (this.state.password === this.state.confirmPassword))
+             {
+
+
             console.log("in email", this.state);
             let requestData = {
                 firstName: this.state.firstName,
@@ -56,11 +88,11 @@ export class Registration extends Component {
                 email: this.state.email,
                 password: this.state.password,
                 confirmPassword: this.state.confirmPassword,
-                service: this.state.service
+               
             }
             console.log("request data", requestData);
-            service.RegisterData( requestData).then((response) => {
-               
+            service.RegisterData(requestData).then((response) => {
+
                 console.log("data", response)
                 if (response.status === 200) {
                     this.props.history.push("/")
@@ -68,77 +100,93 @@ export class Registration extends Component {
             })
                 .catch((error) => {
                     console.log(error);
-                  
+
                 });
-        
+        }
+
     }
     render() {
         return (
-        <div className="containersss">
-        <div className="Registrationcontainer">
-            <div className="fundoonamecontainer">
-                <span className="blue">B</span>
-                <span className="red">o</span>
-                <span className="yellow">o</span>
-                <span className="blue">k</span>
-                <span className="green">-</span>
-                <span className="red">s</span>
-                <span className="green">t</span>
-                <span className="yellow">o</span>
-                <span className="blue">r</span>
-                <span className="red">e</span>
-            </div>
-            <div>
-                <span className="labletext">Create your book store account</span>
-            </div>
-            <div className="TextInputField">
-                <TextField id="outlined-firstName" 
-                label="First Name" type="text"
-                 variant="outlined" size="small"
-                    onChange={this.firstNameHandler}
-                     >first Name</TextField><br />
-                <TextField id="outlined-lastName" label="last Name" type="text" variant="outlined" size="small"
-                    onChange={this.lastNameHandler} >last Name</TextField>
-            </div>
-            <div className="Field">
-                <br /><TextField id="outlined-email" label="Email" type="Email" variant="outlined" size="small"
-                    onChange={this.emailHandler}  fullWidth>Email</TextField><br />
-                <span className="lable"> use my current email address instead</span>
-            </div>
-            <div className="TextInputField">
-                <TextField id="outlined-password" label="password" variant="outlined"
-                    onChange={this.passwordHandler} 
-                    type={this.state.visability ? 'text' : 'password'} size="small" >password</TextField><br />
-                <TextField id="outlined-confirmPassword" label="confirm password" variant="outlined"
-                    onChange={this.confirmPasswordHandler} 
-                    type={this.state.visability ? 'text' : 'password'} size="small"
-                    InputProps={{
-                        endAdornment: (
-                            <div onClick={this.visableIconHandler}>
-                                {this.state.visability ? <VisibilityIcon className="visibility" /> : <VisibilityOffIcon />}
-                            </div>
-                        )
-                    }} >confirm password</TextField>
-            </div>
-            <div>
-                <span className="lable"> use 8 or more charecter with a mix of letters, symbols & numbers </span>
-            </div>
-           
-            <div className="distancnButtons">
-                 <label className="linkages">sign in insted</label>
-                <Button id="contained-button" variant="contained" color="primary" float='right' onClick={this.Register}>submit</Button>
-            </div>
-        </div>
-        <div className='imageContainer'>
-            <div className='imgcontainer'>
-                <img className='image' />
-            </div>
-        </div>
+            <div className="containersss">
+                <div className="Registrationcontainer">
+                    <div className="fundoonamecontainer">
+                        <span className="blue">B</span>
+                        <span className="red">o</span>
+                        <span className="yellow">o</span>
+                        <span className="blue">k</span>
+                        <span className="green">-</span>
+                        <span className="red">s</span>
+                        <span className="green">t</span>
+                        <span className="yellow">o</span>
+                        <span className="blue">r</span>
+                        <span className="red">e</span>
+                    </div>
+                    <div>
+                        <span className="labletext">Create your book store account</span>
+                    </div>
+                    <div className="TextInputField">
+                        <TextField id="outlined-firstName"
+                            label="First Name" type="text"
+                            variant="outlined" size="small"
+                            onChange={this.firstNameHandler}
+                            error={this.state.firstNameError}
+                            helperText={this.state.firstNameError}
+                        >first Name</TextField><br />
+                        <TextField id="outlined-lastName"
+                            label="last Name" type="text"
+                            variant="outlined" size="small"
+                            onChange={this.lastNameHandler}
+                            error={this.state.lastNameError}
+                            helperText={this.state.lastNameError}>last Name</TextField>
+                    </div>
+                    <div className="Field">
+                        <br /><TextField id="outlined-email"
+                            label="Email" type="Email"
+                            variant="outlined" size="small"
+                            onChange={this.emailHandler}
+                            error={this.state.EmailError}
+                            helperText={this.state.EmailError} fullWidth>Email</TextField><br />
+                        <span className="lable"> use my current email address instead</span>
+                    </div>
+                    <div className="TextInputField">
+                        <TextField id="outlined-password" label="password" variant="outlined"
+                            onChange={this.passwordHandler}
+                            error={((this.state.passwordError)||(this.state.MissmatchError))}
+                            helperText={((this.state.passwordError)||(this.state.MissmatchError))}
+                            
+                            type={this.state.visability ? 'text' : 'password'} size="small" >password</TextField><br />
+                        <TextField id="outlined-confirmPassword" label="confirm password" variant="outlined"
+                            onChange={this.confirmPasswordHandler}
+                            error={((this.state.confirmPasswordError)||(this.state.MissmatchError))}
+                            helperText={((this.state.confirmPasswordError)||(this.state.MissmatchError))}
+                            type={this.state.visability ? 'text' : 'password'} size="small"
+                            InputProps={{
+                                endAdornment: (
+                                    <div onClick={this.visableIconHandler}>
+                                        {this.state.visability ? <VisibilityIcon className="visibility" /> : <VisibilityOffIcon />}
+                                    </div>
+                                )
+                            }} >confirm password</TextField>
+                    </div>
+                    <div>
+                        <span className="lable"> use 8 or more charecter with a mix of letters, symbols & numbers </span>
+                    </div>
 
-       
-    </div>
-);
-}
+                    <div className="distancnButtons">
+                        <label className="linkages">sign in insted</label>
+                        <Button id="contained-button" variant="contained" color="primary" float='right' onClick={this.Register}>submit</Button>
+                    </div>
+                </div>
+                <div className='imageContainer'>
+                    <div className='imgcontainer'>
+                        <img className='image' />
+                    </div>
+                </div>
+
+
+            </div>
+        );
+    }
 }
 
 export default Registration;
---
+++ src/configration/regex.jsx
@@ -0,0 +1,6 @@
+class RejexPatterns{
+    static NamePattern = new RegExp(/^[A-Z]{1}[a-z]{2,}$/ );
+    static EmailPattern= new RegExp(/^((?!\.)[\w-_.]*[^.])(@\w+)(\.\w+(\.\w+)?[^.\W])$/);
+    static passwordPattern = new  RegExp( /^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=[^$@!#%*?&]*[$#@!%*?&][^$@!#%*?&]*$).{8,}/);
+}
+export default RejexPatterns;

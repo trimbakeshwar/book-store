bad262169e983311b4d285be37b2a1ac15d63144
trimbakeshwar <hudge.trimbakeshwar@gmail.com>
2020-07-23 20:00:22 +0530

    Added:-scss for cards

--- src/components/userstore/displayCard.jsx
+++ src/components/userstore/displayCard.jsx
@@ -3,19 +3,9 @@ import IconButton from '@material-ui/core/IconButton';
 import React, { Component } from 'react';
 import Card from '@material-ui/core/Card';
 import Masonry from 'react-masonry-css';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
-import { Link } from 'react-router-dom';
-import EditOutlinedIcon from '@material-ui/icons/EditOutlined';
-import DeleteOutlineOutlinedIcon from '@material-ui/icons/DeleteOutlineOutlined';
 
 import { withRouter } from 'react-router';
-
+import "../../stylepage/displayBook.scss"
 import adminService from "../../services/adminServices";
 import ImageOutlinedIcon from '@material-ui/icons/ImageOutlined';
 import { TextareaAutosize } from '@material-ui/core';
